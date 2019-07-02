a41dcd56261103901485b08e48117952b795dc35
maohuitao <maoht@comsenz-service.com>
2019-07-02 11:49:11 +0800

    新url

--- controllers/url.go
+++ controllers/url.go
@@ -26,3 +26,19 @@ func Goto(c *gin.Context) {
 	link, _ := db.Get(url)
 	c.Redirect(302, link)
 }
+
+func Zhuanqu(c *gin.Context) {
+	c.Redirect(302, "http://go.xboxfan.com/goto?url=fd3e716572730d14c045889c3a79fc92")
+}
+
+func Kefu(c *gin.Context) {
+	c.Redirect(302, "http://go.xboxfan.com/goto?url=ee550b7f025a9fd6613d3cd513c2a246")
+}
+
+func Dingyue(c *gin.Context) {
+	c.Redirect(302, "http://go.xboxfan.com/goto?url=aa0254cf4cae61fee4af860852f51a43")
+}
+
+func Yugou(c *gin.Context) {
+	c.Redirect(302, "http://go.xboxfan.com/goto?url=ffc5249d85d55ca1a85c3dc40ce069a8")
+}
--- main.go
+++ main.go
@@ -9,6 +9,11 @@ func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/create/", C.Create)
 	r.GET("/goto/", C.Goto)
+	r.GET("/share/", C.Share)
+	r.GET("/360转区", C.Zhuanqu)
+	r.GET("/客服", C.Kefu)
+	r.GET("/订阅", C.Dingyue)
+	r.GET("/取消预购", C.Yugou)
 	return r
 }
 
